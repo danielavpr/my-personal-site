444ddae7d845d568e35d91952761f0e6729579f4
Daniela Ramón <daniela.perez@wizeline.com>
2021-02-07 17:04:49 -0600

    :rocket: build

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -853,7 +853,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: true,
+      isStatic: false,
       isDev: false,
       isHMR: false,
       app,
@@ -863,9 +863,13 @@ async function setContext(app, context) {
       env: {}
     }; // Only set once
 
-    if (false) {}
+    if ( true && context.req) {
+      app.context.req = context.req;
+    }
 
-    if (false) {}
+    if ( true && context.res) {
+      app.context.res = context.res;
+    }
 
     if (context.ssrContext) {
       app.context.ssrContext = context.ssrContext;
@@ -2130,10 +2134,6 @@ const layouts = {
 
     isFetching() {
       return this.nbFetching > 0;
-    },
-
-    isPreview() {
-      return Boolean(this.$options.previewData);
     }
 
   },
@@ -2581,9 +2581,7 @@ const createNext = ssrContext => opts => {
     routePath: ''
   }; // Remove query from url is static target
 
-  if ( true && ssrContext.url) {
-    ssrContext.url = ssrContext.url.split('?')[0];
-  } // Public runtime config
+  if (false) {} // Public runtime config
 
 
   ssrContext.nuxt.config = ssrContext.runtimeConfig.public; // Create the app definition and the instance (created for each request)
